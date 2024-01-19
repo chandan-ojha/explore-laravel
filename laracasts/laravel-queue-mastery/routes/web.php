76b0bd09e0d91daddafec71b4cb1526f1380ff14
Chandan Ojha <chandanojha776@gmail.com>
2024-01-19 15:10:52 +0600

    #dispatching-and-running-jobs

--- laracasts/laravel-queue-mastery/routes/web.php
+++ laracasts/laravel-queue-mastery/routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    //(new \App\Jobs\SendWelcomeEmail())->handle();
+    \App\Jobs\SendWelcomeEmail::dispatch();
     return view('welcome');
 });
