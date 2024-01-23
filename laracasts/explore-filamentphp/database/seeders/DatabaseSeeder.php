3c3af992f41a03bba4e8379404c28e1c707dc692
Chandan Ojha <chandanojha776@gmail.com>
2024-01-23 11:54:33 +0600

    #models-migrations-factories-stubs-created-by-blueprint

--- laracasts/explore-filamentphp/database/seeders/DatabaseSeeder.php
+++ laracasts/explore-filamentphp/database/seeders/DatabaseSeeder.php
@@ -12,11 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //\App\Models\Conference::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $talks = \App\Models\Talk::factory(5)->create();
+        $talks->each(function ($talk) {
+            $speaker = \App\Models\Speaker::factory()->create();
+            $talk->speaker()->associate($speaker);
+            $talk->save();
+        });
+
     }
 }
