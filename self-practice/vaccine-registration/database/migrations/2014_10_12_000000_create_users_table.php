7e1d4f413764f5eb4a82f6f7a452d65071f39c2d
Chandan Ojha <chandanojha776@gmail.com>
2024-01-23 15:47:51 +0600

    updated

--- self-practice/vaccine-registration/database/migrations/2014_10_12_000000_create_users_table.php
+++ self-practice/vaccine-registration/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('nid', 10)->unique()->nullable();
+            $table->string('phone', 11)->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
