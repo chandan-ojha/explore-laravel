53fead26f49f649c5c2668e99553d0db616a5149
Chandan Ojha <chandanojha776@gmail.com>
2024-01-25 15:21:33 +0600

    #select-input

--- laracasts/explore-filamentphp/app/Models/Venue.php
+++ laracasts/explore-filamentphp/app/Models/Venue.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Enums\Region;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
 
 class Venue extends Model
 {
@@ -12,10 +15,32 @@ class Venue extends Model
 
     protected $casts = [
         'id' => 'integer',
+        'region' => Region::class,
     ];
 
     public function conferences(): HasMany
     {
         return $this->hasMany(Conference::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+            TextInput::make('city')
+                ->required()
+                ->maxLength(255),
+            TextInput::make('country')
+                ->required()
+                ->maxLength(255),
+            TextInput::make('postal_code')
+                ->required()
+                ->maxLength(255),
+            Select::make('region')
+                ->enum(Region::class)
+                ->options(Region::class),
+        ];
     }
 }
--- laracasts/explore-filamentphp/database/seeders/DatabaseSeeder.php
+++ laracasts/explore-filamentphp/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,14 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('1234'),
+        ]);
+
         //\App\Models\Conference::factory(5)->create();
 
-        $talks = \App\Models\Talk::factory(5)->create();
+        /*$talks = \App\Models\Talk::factory(5)->create();
         $talks->each(function ($talk) {
             $speaker = \App\Models\Speaker::factory()->create();
             $talk->speaker()->associate($speaker);
             $talk->save();
-        });
+        });*/
 
     }
 }
