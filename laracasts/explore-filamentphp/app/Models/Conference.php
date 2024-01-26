d53e3269af13f152c938a46595e2b9c864b68a16
Chandan Ojha <chandanojha776@gmail.com>
2024-01-26 11:35:52 +0600

    #layouts

--- laracasts/explore-filamentphp/app/Models/Conference.php
+++ laracasts/explore-filamentphp/app/Models/Conference.php
@@ -3,10 +3,20 @@
 namespace App\Models;
 
 use App\Enums\Region;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Toggle;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Get;
 
 class Conference extends Model
 {
@@ -33,5 +43,69 @@ class Conference extends Model
     public function talks(): BelongsToMany
     {
         return $this->belongsToMany(Talk::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            Section::make('Conference Details')
+                ->collapsible()
+                ->description('Provide some basic information about the conference.')
+                ->icon('heroicon-o-information-circle')
+                ->columns(2)
+                ->schema([
+                    TextInput::make('name')
+                        ->columnSpanFull()
+                        ->label('Conference')
+                        ->default('My Conference')
+                        ->required()
+                        ->maxLength(60),
+                    MarkdownEditor::make('description')
+                        ->columnSpanFull()
+                        ->default('Write Something....')
+                        ->required(),
+                    DateTimePicker::make('start_date')
+                        ->native('false')
+                        ->required(),
+                    DateTimePicker::make('end_date')
+                        ->native('false')
+                        ->required(),
+                    Fieldset::make('Status')
+                        ->columns(1)
+                        ->schema([
+                            Select::make('status')
+                                ->options([
+                                    'draft' => 'Draft',
+                                    'published' => 'Published',
+                                    'archived' => 'Archived',
+                                ])
+                                ->required(),
+                            Toggle::make('is_published')
+                                ->default('true')
+                        ])
+                ]),
+            Section::make('Location')
+                ->columns(2)
+                ->schema([
+                    Select::make('region')
+                        ->live()
+                        ->enum(Region::class)
+                        ->options(Region::class),
+                    Select::make('venue_id')
+                        ->searchable()
+                        ->preload()
+                        ->createOptionForm(Venue::getForm())
+                        ->editOptionForm(Venue::getForm())
+                        ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Get $get) {
+                            return $query->where('region', $get('region'));
+                        })
+                ]),
+            CheckboxList::make('speakers')
+                ->relationship('speakers', 'name')
+                ->options(
+                    Speaker::all()->pluck('name', 'id')->toArray()
+                )
+                ->required(),
+        ];
     }
 }
