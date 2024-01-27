a63c9668137487763f030ade5fddc00bbce0ec39
Chandan Ojha <chandanojha776@gmail.com>
2024-01-27 12:49:11 +0600

    #table-columns

--- laracasts/explore-filamentphp/app/Enums/TalkStatus.php
+++ laracasts/explore-filamentphp/app/Enums/TalkStatus.php
@@ -6,4 +6,12 @@ enum TalkStatus: string {
 case SUBMITTED = 'Submitted';
 case APPROVED = 'Approved';
 case REJECTED = 'Rejected';
+
+    public function getColor(): string {
+        return match ( $this ) {
+            self::SUBMITTED => 'primary',
+            self::APPROVED => 'success',
+            self::REJECTED => 'danger',
+        };
+    }
 }
--- laracasts/explore-filamentphp/app/Filament/Resources/TalkResource.php
+++ laracasts/explore-filamentphp/app/Filament/Resources/TalkResource.php
@@ -2,84 +2,95 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\TalkLength;
 use App\Filament\Resources\TalkResource\Pages;
-use App\Filament\Resources\TalkResource\RelationManagers;
 use App\Models\Talk;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
-class TalkResource extends Resource
-{
+class TalkResource extends Resource {
     protected static ?string $model = Talk::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
-    {
+    public static function form( Form $form ): Form {
         return $form
-            ->schema([
-                Forms\Components\TextInput::make('title')
+            ->schema( [
+                Forms\Components\TextInput::make( 'title' )
                     ->required()
-                    ->maxLength(255),
-                Forms\Components\Textarea::make('abstract')
+                    ->maxLength( 255 ),
+                Forms\Components\Textarea::make( 'abstract' )
                     ->required()
-                    ->maxLength(65535)
+                    ->maxLength( 65535 )
                     ->columnSpanFull(),
-                Forms\Components\Select::make('speaker_id')
-                    ->relationship('speaker', 'name')
+                Forms\Components\Select::make( 'speaker_id' )
+                    ->relationship( 'speaker', 'name' )
                     ->required(),
-            ]);
+            ] );
     }
 
-    public static function table(Table $table): Table
-    {
+    public static function table( Table $table ): Table {
         return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('title')
+            ->columns( [
+                Tables\Columns\TextColumn::make( 'title' )
+                    ->sortable()
+                    ->searchable()
+                    ->description( function ( Talk $record ) {
+                        return Str::limit( $record->abstract, 30 );
+                    } ),
+                Tables\Columns\ImageColumn::make( 'speaker.avatar' )
+                    ->label( 'Speaker Avatar' )
+                    ->circular()
+                    ->defaultImageUrl( function ( $record ) {
+                        return 'https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=' . urlencode( $record->speaker->name );
+                    } ),
+                Tables\Columns\TextColumn::make( 'speaker.name' )
+                    ->sortable()
                     ->searchable(),
-                Tables\Columns\TextColumn::make('speaker.name')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
+                Tables\Columns\ToggleColumn::make( 'new_talk' ),
+                Tables\Columns\TextColumn::make( 'status' )
+                    ->badge()
                     ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->filters([
+                    ->color( function ( $state ) {
+                        return $state->getColor();
+                    } ),
+                Tables\Columns\IconColumn::make( 'length' )
+                    ->icon( function ( $state ) {
+                        return match ( $state ) {
+                            TalkLength::NORMAL    => 'heroicon-o-megaphone',
+                            TalkLength::LIGHTNING => 'heroicon-o-bolt',
+                            TalkLength::KEYNOTE   => 'heroicon-o-key',
+                        };
+                    } ),
+            ] )
+            ->filters( [
                 //
-            ])
-            ->actions([
+            ] )
+            ->actions( [
                 Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
+            ] )
+            ->bulkActions( [
+                Tables\Actions\BulkActionGroup::make( [
                     Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+                ] ),
+            ] );
     }
 
-    public static function getRelations(): array
-    {
+    public static function getRelations(): array {
         return [
             //
         ];
     }
 
-    public static function getPages(): array
-    {
+    public static function getPages(): array {
         return [
-            'index' => Pages\ListTalks::route('/'),
-            'create' => Pages\CreateTalk::route('/create'),
-            'edit' => Pages\EditTalk::route('/{record}/edit'),
+            'index'  => Pages\ListTalks::route( '/' ),
+            'create' => Pages\CreateTalk::route( '/create' ),
+            'edit'   => Pages\EditTalk::route( '/{record}/edit' ),
         ];
     }
 }
