1ddb1c1819078a3bf343c9ac5e9e78f28704e7ec
Chandan Ojha <chandanojha776@gmail.com>
2024-01-27 21:54:33 +0600

    #using-filament-outside-of-panels

--- laracasts/explore-filamentphp/app/Models/Attendee.php
+++ laracasts/explore-filamentphp/app/Models/Attendee.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,5 +13,16 @@ class Attendee extends Model {
 
     public function conference(): BelongsTo {
         return $this->belongsTo( Conference::class );
+    }
+
+    public static function getForm(): array {
+        return [
+            Group::make()->columns( 2 )->schema( [
+                TextInput::make( 'name' )
+                    ->required()->maxLength( 255 ),
+                TextInput::make( 'email' )
+                    ->email()->required()->maxLength( 255 ),
+            ] ),
+        ];
     }
 }
