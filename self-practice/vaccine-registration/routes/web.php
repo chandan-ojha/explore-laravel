51b7e12190770a448254f495a606b1f7774dddf9
Chandan Ojha <chandanojha776@gmail.com>
2024-01-31 10:08:07 +0600

    updated

--- self-practice/vaccine-registration/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\VaccinationSignUpPage;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/vaccination-sign-up', VaccinationSignUpPage::class)->name('sign-up');
