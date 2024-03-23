542180a5b7fe8b8f070d561c2f8d1f5e98ba5b0a
Chandan Ojha <chandanojha776@gmail.com>
2024-03-23 13:09:23 +0600

    #active-navigation-link

--- laracasts/30-days-to-learn-laravel/resources/views/Components/layout.blade.php
+++ laracasts/30-days-to-learn-laravel/resources/views/Components/layout.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -8,15 +8,7 @@
     <title>Home Page</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
+<body class="h-full">
 <div class="min-h-full">
     <nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -28,18 +20,9 @@
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
-                            <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                               aria-current="page">
-                                Home
-                            </a>
-                            <a href="/about"
-                               class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                                About
-                            </a>
-                            <a href="/contact"
-                               class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                                Contact
-                            </a>
+                            <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                            <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                            <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
                         </div>
                     </div>
                 </div>
@@ -98,19 +81,9 @@
         <!-- Mobile menu, show/hide based on menu state. -->
         <div class="md:hidden" id="mobile-menu">
             <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-                   aria-current="page">
-                    Home
-                </a>
-                <a href="/about"
-                   class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">
-                    About
-                </a>
-                <a href="/contact"
-                   class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">
-                    Contact
-                </a>
+                <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
             </div>
             <div class="border-t border-gray-700 pb-3 pt-4">
                 <div class="flex items-center px-5">
@@ -147,5 +120,7 @@
         </div>
     </main>
 </div>
+<!-- Scripts Turbolinks -->
+<script src="http://unpkg.com/turbolinks"></script>
 </body>
 </html>
