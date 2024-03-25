fbb5474d930e8c23eb8969802ccdda9bdbd1b35a
Chandan Ojha <chandanojha776@gmail.com>
2024-03-25 11:13:07 +0600

    #meet-eloquent

--- laracasts/30-days-to-learn-laravel/app/Models/Job.php
+++ laracasts/30-days-to-learn-laravel/app/Models/Job.php
@@ -2,38 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000',
-            ]
-        ];
-    }
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (!$job) {
-            abort(404);
-        }
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
