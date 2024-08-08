4127de3bb6372986a765068c79eed7002dc105b5
Chandan Ojha <chandanojha776@gmail.com>
2024-08-08 16:00:58 +0600

    #service-providers

--- laracasts/modularization/bootstrap/providers.php
+++ laracasts/modularization/bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Modules\Order\Providers\OrderServiceProvider::class,
+    Modules\Product\Providers\ProductServiceProvider::class,
+    Modules\Shipment\Providers\ShipmentServiceProvider::class,
 ];
--- laracasts/modularization/modules/Order/Models/OrderLine.php
+++ laracasts/modularization/modules/Order/Models/OrderLine.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace Modules\Order\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
