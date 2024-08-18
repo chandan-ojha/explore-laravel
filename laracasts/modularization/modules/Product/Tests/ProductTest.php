5170bf52d601b2fa30bc49b9a08c43a7d708dcb0
Chandan Ojha <chandanojha776@gmail.com>
2024-08-18 19:35:43 +0600

    #model-factories

--- laracasts/modularization/modules/Product/Tests/ProductTest.php
+++ laracasts/modularization/modules/Product/Tests/ProductTest.php
@@ -9,8 +9,9 @@ class ProductTest extends TestCase
 {
     public function test_it_creates_a_product(): void
     {
-        $product = new Product();
-        $this->assertTrue(false);
+        $product = Product::factory()->create();
+        $this->assertNotEmpty($product->name);
+        //dd($product);
     }
 
 }
