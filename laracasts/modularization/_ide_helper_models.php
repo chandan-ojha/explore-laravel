b0385a1e64e2aa3a016746dbfc2bc05ef380f931
Chandan Ojha <chandanojha776@gmail.com>
2024-09-02 16:47:56 +0600

    Update _ide_helper_models.php

--- laracasts/modularization/_ide_helper_models.php
+++ laracasts/modularization/_ide_helper_models.php
@@ -64,3 +64,102 @@ namespace App\Models{
 	class User extends \Eloquent {}
 }
 
+namespace Modules\Order\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $total_in_cents
+ * @property string $status
+ * @property string $payment_gateway
+ * @property string $payment_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order wherePaymentGateway($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order wherePaymentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereTotalInCents($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUserId($value)
+ */
+	class Order extends \Eloquent {}
+}
+
+namespace Modules\Order\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property int $product_id
+ * @property int $product_price_in_cents
+ * @property int $quantity
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine query()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereOrderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereProductPriceInCents($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereQuantity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereUpdatedAt($value)
+ */
+	class OrderLine extends \Eloquent {}
+}
+
+namespace Modules\Product\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $price_in_cents
+ * @property int $stock
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Modules\Product\Database\Factories\ProductFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product wherePriceInCents($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereStock($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereUpdatedAt($value)
+ */
+	class Product extends \Eloquent {}
+}
+
+namespace Modules\Shipment\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property string $provider
+ * @property string $provider_shipment_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereOrderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereProvider($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereProviderShipmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereUpdatedAt($value)
+ */
+	class Shipment extends \Eloquent {}
+}
+
