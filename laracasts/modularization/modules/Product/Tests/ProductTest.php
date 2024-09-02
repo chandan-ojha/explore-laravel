6c250dfd4d44cae71c14f194d2e21ca4a42280c6
Chandan Ojha <chandanojha776@gmail.com>
2024-09-02 19:06:20 +0600

    feat: building-a-basic-checkout-flow

--- laracasts/modularization/modules/Product/Tests/ProductTest.php
+++ laracasts/modularization/modules/Product/Tests/ProductTest.php
@@ -2,16 +2,18 @@
 
 namespace Modules\Product\Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Modules\Product\Models\Product;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
-    public function test_it_creates_a_product(): void
+    use DatabaseMigrations;
+
+    public function test_it_creates_a_product()
     {
         $product = Product::factory()->create();
 
         $this->assertNotEmpty($product->name);
     }
-
 }
