45f7f1ac4f3a8f905218c2cfb635bd5949ad758b
Chandan Ojha <chandanojha776@gmail.com>
2024-12-03 13:53:36 +0600

    `episode-01` done

---
+++ laracasts/laravel-api-master-class/app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ApiResponses;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+
+    public function login()
+    {
+        return $this->ok('Hello, Login!');
+    }
+}
