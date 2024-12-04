f7bb34ebdc2427177885f115d376735490f2a862
Chandan Ojha <chandanojha776@gmail.com>
2024-12-04 15:09:48 +0600

    #api-versioning

--- laracasts/laravel-api-master-class/routes/api_v1.php
+++ laracasts/laravel-api-master-class/routes/api_v1.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
+use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,8 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::apiResource('tickets',TicketController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
