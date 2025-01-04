be4eafaa643a7f4ba0ef402fdfc5fc3e4e668bcd
Chandan Ojha <chandanojha776@gmail.com>
2025-01-04 15:25:58 +0600

    #using-and-filtering-nested-resources

--- laracasts/laravel-api-master-class/routes/api_v1.php
+++ laracasts/laravel-api-master-class/routes/api_v1.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthorTicketsController;
 use App\Http\Controllers\Api\V1\TicketController;
-use App\Http\Controllers\Api\V1\UsersController;
+use App\Http\Controllers\Api\V1\AuthorsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +18,8 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
-Route::middleware('auth:sanctum')->apiResource('users', UsersController::class);
+Route::middleware('auth:sanctum')->apiResource('authors', AuthorsController::class);
+Route::middleware('auth:sanctum')->apiResource('authors.tickets', AuthorTicketsController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
