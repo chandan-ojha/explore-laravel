7b1f9851e2426ca4f5a445db526eb53fb179ff1b
Chandan Ojha <chandanojha776@gmail.com>
2025-02-01 15:13:34 +0600

    #Using-Policies-for-User-Authorization

--- laracasts/laravel-api-master-class/app/Http/Controllers/Api/V1/ApiController.php
+++ laracasts/laravel-api-master-class/app/Http/Controllers/Api/V1/ApiController.php
@@ -10,6 +10,8 @@ class ApiController extends Controller
 {
     use ApiResponses;
 
+    protected $policyClass;
+
     public function include(string $relationship): bool
     {
         $param = request()->get('include');
@@ -21,5 +23,10 @@ class ApiController extends Controller
         $includeValues = explode(',', strtolower($param));
 
         return in_array(strtolower($relationship), $includeValues);
+    }
+
+    public function isAble($ability, $targetModel)
+    {
+        return $this->authorize($ability, [$targetModel, $this->policyClass]);
     }
 }
--- laracasts/laravel-api-master-class/app/Http/Controllers/Api/V1/TicketController.php
+++ laracasts/laravel-api-master-class/app/Http/Controllers/Api/V1/TicketController.php
@@ -9,10 +9,14 @@ use App\Http\Requests\Api\V1\UpdateTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
 use App\Models\User;
+use App\Policies\V1\TicketPolicy;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class TicketController extends ApiController
 {
+    protected $policyClass = TicketPolicy::class;
+
     /**
      * Display a listing of the resource.
      */
@@ -64,11 +68,16 @@ class TicketController extends ApiController
         try {
             $ticket = Ticket::findOrFail($ticket_id);
 
+            // policy
+            $this->isAble('update', $ticket);
+
             $ticket->update($request->mappedAttributes());
 
             return new TicketResource($ticket);
         } catch (ModelNotFoundException $exception) {
             return $this->error('Ticket cannot be found.', 404);
+        } catch (AuthorizationException $ex) {
+            return $this->error('You are not authorized to update that resource', 401);
         }
     }
 
